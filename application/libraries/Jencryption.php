4f9ef6df228a61093fae0690c39fcb5489c98cbb
agung susanto <si.agung@yahoo.co.id>
2013-06-06 10:52:14 +0700

    add credits author to jencryption.php

--- application/libraries/Jencryption.php
+++ application/libraries/Jencryption.php
@@ -1,5 +1,29 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
+ * jCryption
+ *
+ * PHP version 5.3
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * Many of the functions in this class are from the PEAR Crypt_RSA package ...
+ * So most of the credits goes to the original creator of this package Alexander Valyalkin
+ * you can get the package under http://pear.php.net/package/Crypt_RSA
+ *
+ * I just changed, added, removed and improved some functions to fit the needs of jCryption
+ *
+ * @author     Daniel Griesser <daniel.griesser@jcryption.org>
+ * @copyright  2011 Daniel Griesser
+ * @license    http://www.php.net/license/3_0.txt PHP License 3.0
+ * @version    1.2
+ * @link       http://jcryption.org/
+ */
+
+/** extends by
  * User: agung
  * Date: 6/4/13
  * Time: 9:47 PM
